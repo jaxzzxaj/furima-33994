6fc48fdb808289b81a4789cf08df5f59923636ba
jaxzzxaj <programyourlife1020@gmail.com>
2021-04-02 17:56:27 +0900

    /パスの設定

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  #ターミナルでcontroller+indexを記述したとき、一緒にrouteを自動で設定してくれた。
   get 'products/index'
+  #"/"のリクエストをされたときに行動するアクションを設定。
+  root to: 'products#index'
+  #テーブルに対して、許可するアクションの設定。
   resources :products, only: :index
 end
