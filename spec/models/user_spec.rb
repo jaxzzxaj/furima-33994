24c3ce8f04b1ad2581881639dd06db95976ded8e
jaxzzxaj <programyourlife1020@gmail.com>
2021-04-07 13:17:04 +0900

    テストコードの記述
    ・起こったエラー
    expectの記述方法は違う
    （日本語→英語表記＜ターミナルで書き方を教えてくれる＞）
    ・expect(@user.errors[:***])とexpect(@user.errors.full.messages)の後の.to includeの記述方法の違い。

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -9,96 +9,96 @@ RSpec.describe User, type: :model do
   describe "新規登録" do
     context "新規登録できる場合" do
       it "すべて入力をする" do
-        # expect(@user).to be_valid
+        expect(@user).to be_valid
       end
     end
 
     context "新規登録できない場合" do
       it "nicknameが空だと不可" do
-        # @user.nickname = ''
-        # @user.valid?
-        # expect(@user.errors[:nickname]).to include('を入力してください')
+        @user.nickname = ''
+        @user.valid?
+        expect(@user.errors.full_messages).to include("Nickname can't be blank")
       end
 
       it "emailが空だと不可" do
-        # @user.email = ''
-        # @user.valid?
-        # expect(@user.errors[:email]).to include('を入力してください')
+        @user.email = ''
+        @user.valid?
+        expect(@user.errors.full_messages).to include("Email can't be blank")
       end
 
       it "emailは重複してはならない" do
-        # @user.save
-        # another_user = FactoryBot.build(:user)
-        # another_user.email = @user.email
-        # another_user.valid?
-        # expect(another_user.errors.full_messages).to include('このメールはもう使われています')
+        @user.save
+        another_user = FactoryBot.build(:user)
+        another_user.email = @user.email
+        another_user.valid?
+        expect(another_user.errors.full_messages).to include("Email has already been taken")
       end
 
       it "emailは＠を含む必要がある" do
-        # @user = build(:user, email: 'aaaaabb')
-        # @user.valid?
-        # expect(@user.errors[:email]).to include('メールには＠が必須です')
+        @user = build(:user, email: 'aaaaabb')
+        @user.valid?
+        expect(@user.errors[:email]).to include("is invalid")
       end
 
       it "passwordが空だと不可" do
-        # @user.password = ''
-        # @user.valid?
-        # expect(@user.errors.full_messages).to include('パスワードは空だと不可です')
+        @user.password = ''
+        @user.valid?
+        expect(@user.errors.full_messages).to include("Password can't be blank")
       end
 
       it "passwordは6文字以上の入力が必要" do
-        # @user.password = '00000'
-        # @user.password_confirmation = '00000'
-        # @user.valid?
-        # expect(@user.errors.full_messages).to include('パスワードは６文字以上での入力が必要です')
+        @user.password = '00000'
+        @user.password_confirmation = '00000'
+        @user.valid?
+        expect(@user.errors.full_messages).to include("Password is too short (minimum is 6 characters)")
       end
 
       it "passwordと確認用passwordの値は一致であること" do
-        # @user = build(:user, encrypted_password: '')
-        # @user.valid?
-        # expect(@user.errors[:password_confirmation]).to include('パスワードと確認用パスワードの値は一致でなければならない')
+        @user = build(:user, encrypted_password: '')
+        @user.valid?
+        expect(@user.errors[:password_confirmation]).to include()
       end
 
       it "名字が空だと不可" do
-        # @user.family_name = ''
-        # @user.valid?
-        # expect(@user.errors.full_messages).to include('名字は空だと不可です')
+        @user.family_name = ''
+        @user.valid?
+        expect(@user.errors.full_messages).to include("Family name can't be blank")
       end
 
       it "名前が空だと不可" do
-        # @user.second_name= ''
-        # @user.valid?
-        # expect(@user.errors.full_messages).to include('名前は空だと不可です')
+        @user.second_name= ''
+        @user.valid?
+        expect(@user.errors.full_messages).to include("Second name can't be blank")
       end
 
       it "カナ表記の名字が空だと不可" do
-        # @user.reader_family_name = ''
-        # @user.valid?
-        # expect(@user.errors.full_messages).to include('カナ表記/名字は空だと不可です')
+        @user.reader_family_name = ''
+        @user.valid?
+        expect(@user.errors.full_messages).to include("Reader family name can't be blank")
       end
 
       it "カナ表記の名前が空だと不可" do
-      #   @user.reader_second_name = ''
-      #   @user.valid?
-      #   expect(@user.errors.full_messages).to include('カナ表記/名前は空だと不可です')
+        @user.reader_second_name = ''
+        @user.valid?
+        expect(@user.errors.full_messages).to include("Reader second name can't be blank")
       end
 
       it "誕生年が空だと不可" do
-        # @user.birth_y = ''
-        # @user.valid?
-        # expect(@user.errors.full_messages).to include('誕生年は空だと不可です')
+        @user.birthday = ''
+        @user.valid?
+        expect(@user.errors.full_messages).to include("Birthday can't be blank")
       end
 
       it "誕生月が空だと不可" do
-        # @user.birth_m = ''
-        # @user.valid?
-        # expect(@user.errors.full_messages).to include('誕生月は空だと不可です')
+        @user.birthday = ''
+        @user.valid?
+        expect(@user.errors.full_messages).to include("Birthday can't be blank")
       end
 
       it "誕生日が空だと不可" do
-        # @user.birth_d= ''
-        # @user.valid?
-        # expect(@user.errors.full_messages).to include('誕生日は空だと不可です')
+        @user.birthday= ''
+        @user.valid?
+        expect(@user.errors.full_messages).to include("Birthday can't be blank")
       end
     end
   end
