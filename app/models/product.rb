b13cd28ee6d71b4052ad1e6d022a705473a73acd
jaxzzxaj <programyourlife1020@gmail.com>
2021-04-10 13:39:36 +0900

    出品の実装１
    ・routesでcreateの追加
    ・controllerの修正とcreate設定
    ・viewのhogeをvalidationに合わせる
    ・viewの画像添付のため、gemの導入
    ・active storage のDBを作成した後、imageをproductとつなげる

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,14 +1,16 @@
 class Product < ApplicationRecord
   #テーブル間のアソシエーション（user_idはpresence: trueしなくともよい＝つながっているからデフォルトでなる）
-  #belongs_to :user
+  belongs_to :user
+  has_one_attached :image
 
-  # with_options presence: true do
-  #   validates :name
-  #   validates :category_id
-  #   validates :shipping_charges_id
-  #   validates :estimated_shipping_date_id
-  #   validates :prefecture
-  #   validates :price
-  #   validates :text
-  # end
+  with_options presence: true do
+    validates :name
+    validates :category_id
+    validates :product_condition_id
+    validates :shipping_charges_id
+    validates :estimated_shipping_date_id
+    validates :prefecture_id
+    validates :price
+    validates :text
+  end
 end
