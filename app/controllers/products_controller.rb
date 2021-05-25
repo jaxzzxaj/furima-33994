1b8332133154b7ee72385e50999ff585a99c30a8
jaxzzxaj <programyourlife1020@gmail.com>
2021-05-25 21:40:03 +0900

    コード修正
    重複しているところをbefore_actionでひとまとめに
    
    urlで遷移するときに遮断する機能をupdateのときにも設定する
    
    コメントアウトの削除

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,6 +1,7 @@
 class ProductsController < ApplicationController
   # 未ログインuserを強制的にログインページへ遷移させる設定
   before_action :authenticate_user!, except: [:index, :show]
+  before_action :find_params, only: [:show, :edit, :update]
 
   # 下記の記述で@productsが新規投稿順に並ぶようにしている。
   def index
@@ -15,7 +16,6 @@ class ProductsController < ApplicationController
   # 下記の記述でnew.htmlでform_withで取得したデータをDBに保存してindexへ遷移するように設定。
   def create
     @product = Product.new(product_params)
-    # @products = @product.includes(:user)
     if @product.save
       redirect_to '/'
     else render :new
@@ -24,20 +24,17 @@ class ProductsController < ApplicationController
 
   # DBに保存したデータをshowに使えるようにしたい。
   def show
-    @product = Product.find(params[:id])
   end
 
   # 編集画面にもデータを持っていくようにする。
   def edit
-    @product = Product.find(params[:id])
     redirect_to '/' unless current_user.id == @product.user_id
   end
 
   # 編集した内容をDBに保存、不備があった場合はeditに戻る
   def update
-    @product = Product.find(params[:id])
     if @product.update(product_params)
-      redirect_to '/'
+      redirect_to '/' unless current_user.id == @product.user_id
     else
       render :edit
     end
@@ -48,5 +45,9 @@ class ProductsController < ApplicationController
   def product_params
     params.require(:product).permit(:name, :category_id, :product_condition_id, :shipping_charges_id, :estimated_shipping_date_id,
                                     :prefecture_id, :price, :text, :image).merge(user_id: current_user.id)
+  end
+
+  def find_params
+    @product = Product.find(params[:id])
   end
 end
