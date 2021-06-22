49f095a2df2a7b664f4c9284232b8c36672e783e
jaxzzxaj <programyourlife1020@gmail.com>
2021-06-22 15:35:14 +0900

    リファクタリング

--- app/javascript/preview.js
+++ app/javascript/preview.js
@@ -1,16 +1,32 @@
 document.addEventListener('DOMContentLoaded', function(){
   if (document.getElementById('message_image')){
   const ImageList = document.getElementById('image-list');
+
   document.getElementById('product_image').addEventListener('change', function(e){
-    const file = e.target.files(0);
-    const blob = window.URL.createObjectURL(file);
 
-    const imageElement = document.createElement('div');
-    const blobImage =document.createElement('img');
-    blobImage.setAttribute('src', blob);
+      // 選択した画像を表示する関数
+      const createImageHTML = (blob) => {
+       // 画像を表示するためのdiv要素を生成
+      const imageElement = document.createElement('div');
 
-    imageElement.appendChild(blobImage);
-    ImageList.appendChild(imageElement);
+      // 表示する画像を生成
+      const blobImage = document.createElement('img');
+      blobImage.setAttribute('src', blob);
+      // 生成したHTMLの要素をブラウザに表示させる
+      imageElement.appendChild(blobImage);
+      ImageList.appendChild(imageElement);
+    };
+
+      const ImageContent = document.querySelector('img');
+      // 画像が表示されている場合のみ、すでに存在している画像を削除する
+      if (imageContent){
+      imageContent.remove();
+    }
+
+      const file = e.target.files(0);
+      const blob = window.URL.createObjectURL(file);
+
+      createImageHTML(blob);
     });
   }
 });
