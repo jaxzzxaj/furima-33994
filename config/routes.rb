7b382e291ee2553bb2e6fe53646428d304afd752
jaxzzxaj <programyourlife1020@gmail.com>
2021-06-25 12:50:02 +0900

    修正1

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,6 @@
 Rails.application.routes.draw do
   #下記のdevise_for :usersはユーザーの登録やログイン、ログアウトの機能は実装されている。
   devise_for :users
-  #ターミナルでcontroller+indexを記述したとき、一緒にrouteを自動で設定してくれた。
-  get 'products/index'
   #"/"のリクエストをされたときに行動するアクションを設定。
   root to: 'products#index'
 
